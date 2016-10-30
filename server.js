39ade14c69c4e58bfa26ad46fd77816f0d0f66ef
kevinpagtakhan <kevpagtakhan@gmail.com>
2016-10-30 14:32:06 -0700

    Authentication middleware

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ var bodyParser = require('body-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
 var dotenv = require('dotenv').load({silent: true});
+var jwt = require('jsonwebtoken');
 var PORT = process.env.PORT || 3000;
 
 var userRoutes = require('./routes/users.js');
+var spacesRoutes = require('./routes/spaces.js');
 
 mongoose.connect(process.env.MONGO_URL, function(err, db){
   console.log(err || 'DATABASE: OK');
@@ -19,6 +21,29 @@ app.use(bodyParser.json());
 app.use(logger('dev'));
 
 app.use('/api/users', userRoutes);
+
+app.use(function(req, res, next){
+  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+  if(token) {
+    jwt.verify(token, 'helloworld', function(err, decoded) {
+      if (err) {
+        return res.json({ success: false, message: 'Token authentication failed.' });
+      } else {
+        req.user = decoded;
+        next();
+      }
+    });
+  } else {
+    res.json({success: false, message: 'No token provided'});
+  }
+})
+
+app.use('/api/spaces', spacesRoutes);
+
+app.get('/', function(req, res){
+  console.log(req.user);
+  res.json({success: true, message: 'OK'})
+});
 
 app.listen(PORT, function(err){
   console.log(err || 'SERVER: OK');
